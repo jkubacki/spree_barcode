e54f3a0407f2cda8ccc894c308e163972bd3f7a9
Jakub Kubacki <kubacki.jk@gmail.com>
2014-07-16 13:40:06 +0200

    Many barcodes at once.

--- app/controllers/spree/admin/barcodes_controller.rb
+++ app/controllers/spree/admin/barcodes_controller.rb
@@ -1,13 +1,33 @@
 class Spree::Admin::BarcodesController < Spree::Admin::BaseController
 
   def variant
-    sku = barcodes_params[:barcode_form][:sku]
-    if sku.blank? then sku = barcodes_params[:barcode_form][:sku_text] end
-    unless Spree::Variant.exists?(sku: sku) then return redirect_to '/admin/barcodes', flash: {error: 'Błędny symbol'} end
-    variant = Spree::Variant.find_by(sku: sku)
+    skus = barcodes_params[:barcode_form][:sku].split
+    if skus.blank? then skus = barcodes_params[:barcode_form][:sku_text].split end
     barcode_generator = BarcodeGenerator.new
-    barcode_generator.barcode_variant(variant.clone, 1, 'small')
-    redirect_to '/admin/barcodes', flash: {success: "Wygenerowano kod kreskowy dla symbolu #{sku}"}
+    errors = []
+    skus.each do |sku|
+      if Spree::Variant.exists?(sku: sku)
+        variant = Spree::Variant.find_by(sku: sku)
+        if variant.is_master?
+          unless variant.product.has_variants?
+            barcode_generator.barcode_variant(variant.clone, 1, 'small')
+            next
+          end
+          variant.product.variants.each do |var|
+            barcode_generator.barcode_variant(var.clone, 1, 'small')
+          end
+        else
+          barcode_generator.barcode_variant(variant.clone, 1, 'small')
+        end
+      else
+        errors << "Błędny symbol #{sku}"
+      end
+    end
+    if errors.blank?
+      redirect_to '/admin/barcodes', flash: {success: "Wygenerowano kod kreskowy dla symboli #{skus.join(' ')}"}
+    else
+      redirect_to '/admin/barcodes', flash: {error: errors.join(' ')}
+    end
   end
 
   def barcodes_params
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Spree::Core::Engine.routes.append do
   namespace :admin do
     # get 'variants/:id/barcodes/:quantity', to: 'barcodes#variant'
     get 'barcodes', to: 'barcodes#index'
+    get '/variants/barcodes', to: 'barcodes#variant'
   end
 end
